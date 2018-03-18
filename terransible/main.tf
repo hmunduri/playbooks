f72e867852c9bf87d40f710fa4fb35343d1a0a66
hmunduri <hmunduri@gmail.com>
2018-03-18 15:33:05 +0000

    Route53

--- terransible/main.tf
+++ terransible/main.tf
@@ -507,3 +507,50 @@ resource "aws_autoscaling_group" "wp_asg" {
     create_before_destroy = true
   }
 }
+
+# -------- Route 53 ----------
+#Primary Zone 
+
+resource "aws_route53_zone" "primary" {
+  name              = "${var.domain_name}.com"
+  delegation_set_id = "${var.delegation_set}"
+}
+
+# WWW
+resource "aws_route53_record" "www" {
+  zone_id = "${aws_route53_zone.primary.zone_id}"
+  name    = "www.${var.domain_name}.com"
+  type    = "A"
+
+  alias {
+    name                   = "${aws_elb.wp_elb.dns_name}"
+    zone_id                = "{aws_elb.wp_elb.zone_id}"
+    evaluate_target_health = false
+  }
+}
+
+#DEV 
+
+resource "aws_route53_record" "dev" {
+  zone_id = "${aws_route53_zone.primary.zone_id}"
+  name    = "dev.${var.domain_name}.com"
+  type    = "A"
+  ttl     = "300"
+  records = ["${aws_instance.wp_dev.public_ip}"]
+}
+
+# Secondary Zone ( Private Zone)
+
+resource "aws_route53_zone" "secondary" {
+  name   = "${var.domain_name}.com"
+  vpc_id = "${aws_vpc.wp_vpc.id}"
+}
+
+# DB
+resource "aws_route53_record" "db" {
+  zone_id = "${aws_route53_zone.secondary.zone_id}"
+  name    = "db.${var.domain_name}.com"
+  type    = "CNAME"
+  ttl     = "300"
+  records = ["${aws_db_instance.wp_db.address}"]
+}
